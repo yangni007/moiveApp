9a4f35d180f41855f5396ce1bc772ed2bdc0e894
yangni <yangni@iyunxiao.com>
2018-12-16 20:13:34 +0800

    '写评论页面'

---
+++ src/components/user/writeComment.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from 'react-dom'
+
+import { BrowserRouter,  Route, Switch, withRouter} from 'react-router-dom';
+
+import { NavBar, Icon } from 'antd-mobile';
+
+class WriteComment extends React.Component{
+    back = () => {
+        this.props.history.push('/', {active: 2})
+    }
+    render() {
+        return (
+            <div>
+                <NavBar
+                    mode="dark"
+                    icon={<Icon type="left" />}
+                    onLeftClick={this.back}
+                    >写评论</NavBar>
+            </div>
+        )
+    }
+}
+
+export default WriteComment
+// export default withRouter(WriteComment)
