f5bba7ca4b8f48257a2cf69dfbae9d2209dcafac
yangni <yangni@iyunxiao.com>
2019-01-06 00:13:57 +0800

    '拖动'

--- src/components/user/drag.jsx
+++ src/components/user/drag.jsx
@@ -12,23 +12,45 @@ export default class UserCenter extends React.Component{
         offset: {
             left: 0,
             top: 0
-        }
+        },
+        dragFlag: false
     }
     mouseDownEvent = (e) => {
-        console.log(e)
+        e.persist()
         let offset = {
             left: e.clientX,
             top: e.clientY
         }
-        this.setState({offset: offset})
+        this.setState({offset: offset, dragFlag: true})
     }
     mouseMoveEvent = (e) => {
-        
+        e.persist()
+        if(this.state.dragFlag) {
+            let offset = this.state.offset,
+                moveX = e.clientX - offset.left,
+                moveY = e.clientY - offset.top,
+                left = e.target.offsetLeft,
+                top = e.target.offsetTop;
+            e.target.style.left = left + moveX + 'px';
+            e.target.style.top = top + moveY + 'px';
+            offset = {
+                left: e.clientX,
+                top: e.clientY
+            }
+            this.setState({offset: offset})
+        }
+    }
+    mouseUpEvent = (e) => {
+        this.setState({dragFlag: false})
     }
     render() {
         return (
             <div>
-                <div className="box" onMouseDown={this.mouseDownEvent}></div>
+                <div 
+                className="box" 
+                onMouseDown={this.mouseDownEvent}
+                onMouseMove={this.mouseMoveEvent}
+                onMouseUp={this.mouseUpEvent}></div>
             </div>
             
         )
