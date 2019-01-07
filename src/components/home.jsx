dd64cdc3418c9d795bbbe16d38166172c277f092
yangni <yangni@iyunxiao.com>
2019-01-07 10:52:47 +0800

    首页样式调整

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,14 +4,17 @@ import { Link} from 'react-router-dom'
 import { List } from 'antd-mobile';
 
 const Item = List.Item;
+require('../asset/css/home.scss')
 
 class Home extends React.Component {
     
     
     render() {
         return (
-            <div>
-                <h1 className='main'>目录</h1>
+            <div className="home">
+                <div className="main">
+                    <div className="title">目录</div> 
+                </div>
                 <List>
                     <Link  to="/movie">
                         <Item arrow="horizontal" >电影</Item>
