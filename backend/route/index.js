8bb706aba2470cf63ee11a5df67b144a3dcddd2e
yangni <yangni@iyunxiao.com>
2019-01-09 18:54:07 +0800

    添加bodyparse

--- backend/route/index.js
+++ backend/route/index.js
@@ -1,11 +1,7 @@
 
 function initRouter(app) {
+
     
-    app.get('/login', (req, res) => {
-        console.log('登陆接口')
-        res.send('欢迎，登陆接口')
-        res.end()
-    })
     
 }
 
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,22 @@ const express = require('express');
 const router = require('./route/index')
 const app = express()
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.raw({ limit: '10mb' }));
+
 app.all('*', function(req, res, next){
     console.log('接口')
     next()
 })
-router.initRouter(app)
+app.post('/login', (req, res) => {
+    console.log(req.body)
+    res.send()
+    
+    res.end()
+})
+// router.initRouter(app)
 
 
 app.listen(3000, () => {
